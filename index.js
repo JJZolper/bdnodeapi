e2bbb79c31fb2c99b72616e10ae77dd456527c10
JJ Zolper <jjz@vt.edu>
2017-06-29 23:26:59 -0400

    First iteration of working index create and retrieval

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-var bonsai_url = process.env.BONSAI_URL; 
-
 var request = require('request');
 var elasticsearch = require('elasticsearch');
 var express = require('express');
+var fs = require('fs');
 var app = express();
 
-var client = new elasticsearch.Client({host: bonsai_url, log: 'trace'});
+// console.log(process.env);
+var connectionString = process.env.SEARCHBOX_URL;
+
+var client = new elasticsearch.Client({
+    host: connectionString
+});
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -16,17 +20,33 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, resp) {
-  client.get({
-    index: 'user',
-    type: 'doctor'
-  }, function (error, resp) {
-    console.log(resp);
+
+  var result = [];
+
+  client.search({
+      index: 'person',
+      type: 'doctor',
+      body: {
+          query: {
+              match: {
+                uid: "ebfca2b339d1e5e23ce0af3ab1c886f4"
+              }
+          }
+      }
+  }).then(function (data) {
+      // console.log(data);
+      // console.log(data.hits.hits);
+      result = data.hits.hits;
+      resp.send(JSON.stringify(result[0]));
+  }, function (err) {
+      console.log(err.message);
   });
+
 });
 
 const api_key = '0e0a2cf386c18f688b9dc56ed67238bd';
 const options = {  
-    url: 'https://api.betterdoctor.com/2016-03-01/doctors?name=mike%20Nichols&user_key=' + api_key,
+    url: 'https://api.betterdoctor.com/2016-03-01/doctors?name=Mike%20Nichols&user_key=' + api_key,
     method: 'GET',
     headers: {
         'Accept': 'application/json',
@@ -40,18 +60,34 @@ app.get('/api/v1/doctors/search', function(req, resp) {
       let json = JSON.parse(body);
       let result = json['data'];
 
-      client.create({
-        index: 'user',
-        type: 'doctor',
-        id: json['data']['uid'],
-        body: json['data']
-      }, function (error, response) {
-        // ...
+      /*
+      fs.writeFile("./sample.txt", JSON.stringify(json['data']), (err) => {
+          if (err) {
+              console.error(err);
+              return;
+          };
+          console.log("File has been created");
       });
-      console.log(json['data']);
+      */
+
+      console.log(json['data'][0]);
+      console.log(json['data'][0]['uid']);
+
+      var id = json['data'][0]['uid'];
+      var document = json['data'][0];
+
+      client.index({
+        index: 'person',
+        type: 'doctor',
+        id: id,
+        body: document
+      }, function (error, response) {
+        console.log(response);
+      });
+
+      resp.send("Success!")
+
   });
-
-
 
 });
 
@@ -59,4 +95,9 @@ app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
+function isNotEmptyObject(obj) {
+  if(Object.keys(obj).length > 0){
+    return true;
+  }
+}
 
